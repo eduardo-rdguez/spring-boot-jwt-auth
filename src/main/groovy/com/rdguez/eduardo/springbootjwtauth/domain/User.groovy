c962c68ac03033e6bd8343f3430aa2813820bc81
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-10-17 19:00:45 -0500

    Add user domain

---
+++ src/main/groovy/com/rdguez/eduardo/springbootjwtauth/domain/User.groovy
@@ -0,0 +1,19 @@
+package com.rdguez.eduardo.springbootjwtauth.domain
+
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToMany
+
+@Entity
+class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id
+  String username
+  String password
+  @ManyToMany(fetch = FetchType.EAGER)
+  List<Role> roles
+}

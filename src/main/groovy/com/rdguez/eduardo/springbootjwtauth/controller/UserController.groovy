b8b4cbf26c0209ceea1c374cafe1182711fc1e42
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-10-17 20:12:01 -0500

    Add enpoints to save users and roles

--- src/main/groovy/com/rdguez/eduardo/springbootjwtauth/controller/UserController.groovy
+++ src/main/groovy/com/rdguez/eduardo/springbootjwtauth/controller/UserController.groovy
@@ -1,12 +1,17 @@
 package com.rdguez.eduardo.springbootjwtauth.controller
 
+import com.rdguez.eduardo.springbootjwtauth.domain.Role
 import com.rdguez.eduardo.springbootjwtauth.domain.User
 import com.rdguez.eduardo.springbootjwtauth.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,4 +26,17 @@ class UserController {
     ResponseEntity.ok().body(users)
   }
 
+  @PostMapping
+  ResponseEntity<User> saveUser(@RequestBody User user) {
+    String requestUri = ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString()
+    User newUser = userService.saveUser(user)
+    ResponseEntity.created(URI.create(requestUri)).body(newUser)
+  }
+
+  @PostMapping("/roles")
+  ResponseEntity<Role> saveRole( @RequestParam String roleName, @RequestParam String userName) {
+    String requestUri = ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString()
+    Role newRole = userService.assignRoleToUser(roleName, userName)
+    ResponseEntity.created(URI.create(requestUri)).body(newRole)
+  }
 }

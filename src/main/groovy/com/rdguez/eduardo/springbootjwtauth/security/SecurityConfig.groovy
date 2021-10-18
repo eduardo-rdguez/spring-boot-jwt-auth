32e2523e8402c8579b391057072c1f0632247090
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-10-17 21:15:49 -0500

    Update method to configure http security

--- src/main/groovy/com/rdguez/eduardo/springbootjwtauth/security/SecurityConfig.groovy
+++ src/main/groovy/com/rdguez/eduardo/springbootjwtauth/security/SecurityConfig.groovy
@@ -1,10 +1,13 @@
 package com.rdguez.eduardo.springbootjwtauth.security
 
+import com.rdguez.eduardo.springbootjwtauth.filter.CustomAuthFilter
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
@@ -22,6 +25,14 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    super.configure(http)
+    http.csrf().disable()
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    http.authorizeRequests().anyRequest().permitAll()
+    http.addFilter(new CustomAuthFilter(authenticationManager()))
+  }
+
+  @Override
+  protected AuthenticationManager authenticationManager() throws Exception {
+    return super.authenticationManager()
   }
 }

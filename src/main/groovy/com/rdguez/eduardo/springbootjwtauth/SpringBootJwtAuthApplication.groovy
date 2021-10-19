9b11c40f42d5499e8d26eb358916a9a373e4bb3b
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-10-18 21:45:28 -0500

    Building a simple spring project

---
+++ src/main/groovy/com/rdguez/eduardo/springbootjwtauth/SpringBootJwtAuthApplication.groovy
@@ -0,0 +1,13 @@
+package com.rdguez.eduardo.springbootjwtauth
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootJwtAuthApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAuthApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/rdguez/eduardo/springbootjwtauth/SpringBootJwtAuthApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.rdguez.eduardo.springbootjwtauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

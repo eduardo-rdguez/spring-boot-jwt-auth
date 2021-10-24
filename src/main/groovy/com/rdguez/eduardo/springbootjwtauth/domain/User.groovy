d6f27a2e383e6ffe946c422c8c0f905708690283
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2021-10-24 18:17:44 -0500

    Remove table name specification

--- src/main/groovy/com/rdguez/eduardo/springbootjwtauth/domain/User.groovy
+++ src/main/groovy/com/rdguez/eduardo/springbootjwtauth/domain/User.groovy
@@ -6,10 +6,8 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.ManyToMany
-import javax.persistence.Table
 
 @Entity
-@Table(name = "USERS")
 class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
